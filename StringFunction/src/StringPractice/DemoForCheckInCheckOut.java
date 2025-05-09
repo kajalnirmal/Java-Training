cb49d1ff1eb67c4218ff015d36572354040e6b33
kajalnirmal <kajalnirmal71@gmail.com>
2025-05-09 18:22:17 +0530

    NEW FILE

---
+++ StringFunction/src/StringPractice/DemoForCheckInCheckOut.java
@@ -0,0 +1,7 @@
+package StringPractice;
+
+public class DemoForCheckInCheckOut {
+    public static void main(String[] args) {
+        System.out.println ("DEMO FOR CHECK IN AND CHECK OUT PROCESS");
+    }
+}
