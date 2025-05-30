55a50700dfdd65c85335ecbe52d649b0dff7111b
kajalnirmal <kajalnirmal71@gmail.com>
2025-05-30 18:17:26 +0530

    NEW files uploded

---
+++ PracticeProgram/src/javabasics/SingleInheritence.java
@@ -0,0 +1,18 @@
+package javabasics;
+class Animal{
+    void eat(){
+        System.out.println (" this animal eats food .");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println ("the dog barks.");
+    }
+}
+public class SingleInheritence {
+    public static void main(String[] args) {
+        Dog d = new Dog ();
+        d.eat ();
+        d.bark ();
+    }
+}
